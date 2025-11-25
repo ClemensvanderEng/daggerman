1b642c5c7be749b24eec032d69bb29fa28ee1b33
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-11-25 11:05:23 +0000

    map gen standardisation and lv 2

--- gen.py
+++ gen.py
@@ -4,7 +4,6 @@ import random
 # Generated room tracking
 Lx = []
 Ly = []
-Lid = []
 
 def create_square(new_x, new_y, new_id, new_genID):
     map.x.append(new_x)
@@ -15,13 +14,27 @@ def create_square(new_x, new_y, new_id, new_genID):
 def start():
     Lx.append(0)
     Ly.append(0)
-    Lid.append("start")
     create_square(0, 0, "x", 0)
     create_square(1, 0, "□", 0)
     create_square(-1, 0, "□", 0)
     create_square(0, 1, "C", 0)
     create_square(2, 0, "□", 4)
     create_square(-2, 0, "□", 1)
+
+def create_mon(new_id):
+    if new_id != "□" and new_id != "C":
+        if new_id == "K":
+            create_monster(4, 3, 0, 2, "K")
+        elif new_id == "S":
+            create_monster(2, 3, 0, 1, "S")
+        elif new_id == "F":
+            create_monster(6, 6, 0, 3, "F")
+        elif new_id == "L":
+            create_monster(10, 3, 1, 4, "L")
+        elif new_id == "O":
+            create_monster(10, 8, 2, 6, "O")
+        elif new_id == "T":
+            create_monster(16, 6, 3, 7, "T")
 
 def create_monster(new_hp, new_damage, new_defense, new_exp, new_id):
     map.Mhp.append(new_hp)
@@ -30,6 +43,175 @@ def create_monster(new_hp, new_damage, new_defense, new_exp, new_id):
     map.Mexp.append(new_exp)
     map.Mid.append(new_id)
     map.MplaceID.append(len(map.id)-1) 
+
+def emptyT(xc, yc):
+    create_square(xc, yc, "□", 0)
+    create_square(xc, yc+1, "□", 0)
+    create_square(xc, yc-1, "□", 0)
+    create_square(xc, yc+2, "□", 3)
+    create_square(xc, yc-2, "□", 2)
+    create_square(xc+1, yc, "□", 0)
+    create_square(xc-1, yc, "□", 0)
+    create_square(xc+2, yc, "□", 4)
+    create_square(xc-2, yc, "□", 1)
+
+def central(xc, yc, Mc, M1, M2):
+    create_square(xc, yc, Mc, 0)
+    create_mon(Mc)
+    create_square(xc, yc+1, "□", 0)
+    create_square(xc, yc-1, "□", 0)
+    create_square(xc, yc+2, "□", 3)
+    create_square(xc, yc-2, "□", 2)
+    create_square(xc+1, yc, "□", 0)
+    create_square(xc-1, yc, "□", 0)
+    create_square(xc+2, yc, "□", 4)
+    create_square(xc-2, yc, "□", 1)
+    create_square(xc-1, yc-1, M1, 0)
+    create_mon(M1)
+    create_square(xc-1, yc+1, M2, 0)
+    create_mon(M2)
+    create_square(xc+1, yc-1, M2, 0)
+    create_mon(M2)
+    create_square(xc+1, yc+1, M1, 0)
+    create_mon(M1)
+
+def chest_allyH(xc, yc, M):
+    create_square(xc, yc, "□", 0)
+    create_square(xc, yc+1, "C", 0)
+    create_square(xc+1, yc, M, 0)
+    create_mon(M)
+    create_square(xc-1, yc, M, 0)
+    create_mon(M)
+    create_square(xc+2, yc, "□", 4)
+    create_square(xc-2, yc, "□", 1)
+
+def chest_allyV(xc, yc, M):
+    create_square(xc, yc, "□", 0)
+    create_square(xc+1, yc, M, 0)
+    create_mon(M)
+    create_square(xc, yc+1, "C", 0)
+    create_square(xc-1, yc, M, 0)
+    create_mon(M)
+    create_square(xc, yc+2, "□", 3)
+    create_square(xc, yc-2, "□", 2)
+
+def chestroomH(xc, yc, M1, M2, gen):
+    if gen == 1:
+        xc2 = xc-5
+        if (xc2/5, yc/5) in zip(Lx, Ly):
+            able = False
+        else:
+            able = True
+            Lx.append(xc2/5)
+            Ly.append(yc/5)
+    if gen == 4:
+        xc2 = xc+5
+        if (xc2/5, yc/5) in zip(Lx, Ly):
+            able = False
+        else:
+            Lx.append(xc2/5)
+            Ly.append(yc/5)
+            able = True
+            save = xc
+            xc = xc2
+            xc2 = save
+    if able == True:
+        create_square(xc, yc, M1, 0)
+        create_mon(M1)
+        create_square(xc, yc-1, M2, 0)
+        create_mon(M2)
+        create_square(xc, yc+1, M2, 0)
+        create_mon(M2)
+        create_square(xc-1, yc, "□", 0)
+        create_square(xc+1, yc, "□", 0)
+        create_square(xc+2, yc, "□", 4)
+        create_square(xc-2, yc, "□", 0)
+        create_square(xc-1, yc-1, "□", 0)
+        create_square(xc-1, yc+1, "□", 0)
+        create_square(xc+1, yc-1, "□", 0)
+        create_square(xc+1, yc+1, "□", 0)
+        create_square(xc-2, yc-1, "C", 0)
+        create_square(xc-2, yc+1, "□", 0)
+        create_square(xc2, yc, M1, 0)
+        create_mon(M1)
+        create_square(xc2, yc-1, M2, 0)
+        create_mon(M2)
+        create_square(xc2, yc+1, M2, 0)
+        create_mon(M2)
+        create_square(xc2-1, yc, "□", 0)
+        create_square(xc2+1, yc, "□", 0)
+        create_square(xc2+2, yc, "□", 0)
+        create_square(xc2-2, yc, "□", 1)
+        create_square(xc2-1, yc-1, "□", 0)
+        create_square(xc2-1, yc+1, "□", 0)
+        create_square(xc2+1, yc-1, "□", 0)
+        create_square(xc2+1, yc+1, "□", 0)
+        create_square(xc2+2, yc-1, "C", 0)
+        create_square(xc2+2, yc+1, "□", 0)
+    else:
+        if M2 != "□" and M2 != "C":
+            chest_allyH(xc, yc, M2)
+        else:
+            chest_allyH(xc, yc, M1)
+
+def chestroomV(xc, yc, M1, M2, gen):
+    if gen == 2:
+        yc2 = yc-5
+        if (xc/5, yc2/5) in zip(Lx, Ly):
+            able = False
+        else:
+            able = True
+            Lx.append(xc/5)
+            Ly.append(yc2/5)
+    if gen == 3:
+        yc2 = yc+5
+        if (xc/5, yc2/5) in zip(Lx, Ly):
+            able = False
+        else:
+            Lx.append(xc/5)
+            Ly.append(yc2/5)
+            able = True
+            save = yc
+            yc = yc2
+            yc2 = save
+    if able == True:
+        create_square(xc, yc, M1, 0)
+        create_mon(M1)
+        create_square(xc-1, yc, M2, 0)
+        create_mon(M2)
+        create_square(xc+1, yc, M2, 0)
+        create_mon(M2)
+        create_square(xc, yc-1, "□", 0)
+        create_square(xc, yc+1, "□", 0)
+        create_square(xc, yc+2, "□", 3)
+        create_square(xc, yc-2, "□", 0)
+        create_square(xc-1, yc-1, "□", 0)
+        create_square(xc-1, yc+1, "□", 0)
+        create_square(xc+1, yc-1, "□", 0)
+        create_square(xc+1, yc+1, "□", 0)
+        create_square(xc-1, yc-2, "C", 0)
+        create_square(xc+1, yc-2, "□", 0)
+        create_square(xc, yc2, M1, 0)
+        create_mon(M1)
+        create_square(xc-1, yc2, M2, 0)
+        create_mon(M2)
+        create_square(xc+1, yc2, M2, 0)
+        create_mon(M2)
+        create_square(xc, yc2-1, "□", 0)
+        create_square(xc, yc2+1, "□", 0)
+        create_square(xc, yc2+2, "□", 0)
+        create_square(xc, yc2-2, "□", 2)
+        create_square(xc-1, yc2-1, "□", 0)
+        create_square(xc-1, yc2+1, "□", 0)
+        create_square(xc+1, yc2-1, "□", 0)
+        create_square(xc+1, yc2+1, "□", 0)
+        create_square(xc-1, yc2+2, "C", 0)
+        create_square(xc+1, yc2+2, "□", 0)
+    else:
+        if M2 != "□" and M2 != "C":
+            chest_allyV(xc, yc, M2)
+        else:
+            chest_allyV(xc, yc, M1)
 
 def gen(gen):
     map.genID[map.id.index("x")] = 0
@@ -67,163 +249,57 @@ def gen(gen):
         if map.lvl == 1:
             room = random.randint(1,6)
             if room == 1:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
+                emptyT(xcentre,ycentre)
             elif room == 2:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre, ycentre+1, "C", 0)
-                create_square(xcentre+1, ycentre, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre-1, ycentre, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
+                chest_allyH(xcentre, ycentre, "K")
             elif room == 3:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre-1, ycentre, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
+                central(xcentre, ycentre, "□", "S", "S")
             elif room == 4:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre+1, ycentre+1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre-1, ycentre-1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
+                central(xcentre, ycentre, "□", "□", "K")
             elif room == 5:
-                create_square(xcentre, ycentre, "C", 0)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                if random.randint(1,2) == 1:
-                    create_square(xcentre, ycentre+2, "□", 3)
-                else:
-                    create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre+1, ycentre+1, "□", 0)
-                create_square(xcentre-1, ycentre-1, "□", 0)
-                create_square(xcentre+1, ycentre-1, "□", 0)
-                create_square(xcentre-1, ycentre+1, "□", 0)
+                chest_allyH(xcentre, ycentre, "□")
             elif room == 6:
-                create_square(xcentre, ycentre, "C", 0)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre+1, ycentre-1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre-1, ycentre+1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre+1, ycentre+1, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre-1, ycentre-1, "S", 0)
-                create_monster(2,3,0,1,"S")
+                central(xcentre, ycentre, "C", "S", "K")
+        elif map.lvl == 2:
+            room = random.randint(1,6)
+            if room == 1:
+                chestroomH(xcentre, ycentre, "□", "F", gen)
+            elif room == 2:
+                emptyT(xcentre,ycentre)
+            elif room == 3:
+                central(xcentre, ycentre, "C", "K", "L")
+            elif room == 4:
+                central(xcentre, ycentre, "L", "□", "□")
+            elif room == 5:
+                chest_allyH(xcentre, ycentre, "L")
+            elif room == 6:
+                central(xcentre, ycentre, "F", "□", "□")
     if gen == 2 or gen == 3:
         if map.lvl == 1:
             room = random.randint(1,6)
             if room == 1:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
+                emptyT(xcentre,ycentre)
             elif room == 2:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre+1, ycentre, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre, ycentre+1, "C", 0)
-                create_square(xcentre-1, ycentre, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
+                chest_allyV(xcentre, ycentre, "K")
             elif room == 3:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre+1, ycentre, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre, ycentre+1, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre, ycentre-1, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
+                central(xcentre, ycentre, "□", "S", "S")
             elif room == 4:
-                create_square(xcentre, ycentre, "□", 0)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre+1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre-1, ycentre-1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
+                central(xcentre, ycentre, "□", "□", "K")
             elif room == 5:
-                create_square(xcentre, ycentre, "C", 0)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                if random.randint(1,2) == 1:
-                    create_square(xcentre+2, ycentre, "□", 4)
-                else:
-                    create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre+1, "□", 0)
-                create_square(xcentre-1, ycentre-1, "□", 0)
-                create_square(xcentre+1, ycentre-1, "□", 0)
-                create_square(xcentre-1, ycentre+1, "□", 0)
+                chest_allyV(xcentre, ycentre, "□")
             elif room == 6:
-                create_square(xcentre, ycentre, "C", 0)
-                create_square(xcentre+1, ycentre, "□", 0)
-                create_square(xcentre-1, ycentre, "□", 0)
-                create_square(xcentre+2, ycentre, "□", 4)
-                create_square(xcentre-2, ycentre, "□", 1)
-                create_square(xcentre, ycentre+1, "□", 0)
-                create_square(xcentre, ycentre-1, "□", 0)
-                create_square(xcentre, ycentre+2, "□", 3)
-                create_square(xcentre, ycentre-2, "□", 2)
-                create_square(xcentre+1, ycentre-1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre-1, ycentre+1, "K", 0)
-                create_monster(4, 2, 0, 2, "K")
-                create_square(xcentre+1, ycentre+1, "S", 0)
-                create_monster(2,3,0,1,"S")
-                create_square(xcentre-1, ycentre-1, "S", 0)
-                create_monster(2,3,0,1,"S")
+                central(xcentre, ycentre, "C", "S", "K")
+        if map.lvl == 2:
+            room = random.randint(1,6)
+            if room == 1:
+                chestroomV(xcentre, ycentre, "□", "F", gen)
+            elif room == 2:
+                emptyT(xcentre,ycentre)
+            elif room == 3:
+                central(xcentre, ycentre, "C", "K", "L")
+            elif room == 4:
+                central(xcentre, ycentre, "L", "□", "□")
+            elif room == 5:
+                chest_allyV(xcentre, ycentre, "L")
+            elif room == 6:
+                central(xcentre, ycentre, "F", "□", "□")
