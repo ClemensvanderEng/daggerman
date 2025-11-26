3d5da00490482f11818012fa0886614806f59637
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-11-26 10:09:49 +0000

    less gen code

--- gen.py
+++ gen.py
@@ -79,27 +79,27 @@ def central(xc, yc, Mc, M1, M2):
     create_square(xc+1, yc+1, M1, 0)
     create_mon(M1)
 
-def chest_allyH(xc, yc, M):
-    create_square(xc, yc, "□", 0)
-    create_square(xc, yc+1, "C", 0)
-    create_square(xc+1, yc, M, 0)
-    create_mon(M)
-    create_square(xc-1, yc, M, 0)
-    create_mon(M)
-    create_square(xc+2, yc, "□", 4)
-    create_square(xc-2, yc, "□", 1)
+def chest_ally(xc, yc, M, gen):
+    if gen == 1 or gen == 4:
+        create_square(xc, yc, "□", 0)
+        create_square(xc, yc+1, "C", 0)
+        create_square(xc+1, yc, M, 0)
+        create_mon(M)
+        create_square(xc-1, yc, M, 0)
+        create_mon(M)
+        create_square(xc+2, yc, "□", 4)
+        create_square(xc-2, yc, "□", 1)
+    if gen == 2 or gen == 3:
+        create_square(xc, yc, "□", 0)
+        create_square(xc, yc+1, M, 0)
+        create_mon(M)
+        create_square(xc-1, yc, "C", 0)
+        create_square(xc, yc-1, M, 0)
+        create_mon(M)
+        create_square(xc, yc+2, "□", 3)
+        create_square(xc, yc-2, "□", 2)
 
-def chest_allyV(xc, yc, M):
-    create_square(xc, yc, "□", 0)
-    create_square(xc, yc+1, M, 0)
-    create_mon(M)
-    create_square(xc-1, yc, "C", 0)
-    create_square(xc, yc-1, M, 0)
-    create_mon(M)
-    create_square(xc, yc+2, "□", 3)
-    create_square(xc, yc-2, "□", 2)
-
-def chestroomH(xc, yc, M1, M2, gen):
+def chestroom(xc, yc, M1, M2, gen):
     if gen == 1:
         xc2 = xc-5
         if (xc2/5, yc/5) in zip(Lx, Ly):
@@ -108,6 +108,25 @@ def chestroomH(xc, yc, M1, M2, gen):
             able = True
             Lx.append(xc2/5)
             Ly.append(yc/5)
+    if gen == 2:
+        yc2 = yc-5
+        if (xc/5, yc2/5) in zip(Lx, Ly):
+            able = False
+        else:
+            able = True
+            Lx.append(xc/5)
+            Ly.append(yc2/5)
+    if gen == 3:
+        yc2 = yc+5
+        if (xc/5, yc2/5) in zip(Lx, Ly):
+            able = False
+        else:
+            Lx.append(xc/5)
+            Ly.append(yc2/5)
+            able = True
+            save = yc
+            yc = yc2
+            yc2 = save
     if gen == 4:
         xc2 = xc+5
         if (xc2/5, yc/5) in zip(Lx, Ly):
@@ -119,7 +138,13 @@ def chestroomH(xc, yc, M1, M2, gen):
             save = xc
             xc = xc2
             xc2 = save
-    if able == True:
+    if able == False:
+        if M2 != "□" and M2 != "C":
+            chest_ally(xc, yc, M2, gen)
+        else:
+            chest_ally(xc, yc, M1, gen)
+        return
+    if gen == 1 or gen == 4:
         create_square(xc, yc, M1, 0)
         create_mon(M1)
         create_square(xc, yc-1, M2, 0)
@@ -152,33 +177,7 @@ def chestroomH(xc, yc, M1, M2, gen):
         create_square(xc2+1, yc+1, "□", 0)
         create_square(xc2+2, yc-1, "C", 0)
         create_square(xc2+2, yc+1, "□", 0)
-    else:
-        if M2 != "□" and M2 != "C":
-            chest_allyH(xc, yc, M2)
-        else:
-            chest_allyH(xc, yc, M1)
-
-def chestroomV(xc, yc, M1, M2, gen):
-    if gen == 2:
-        yc2 = yc-5
-        if (xc/5, yc2/5) in zip(Lx, Ly):
-            able = False
-        else:
-            able = True
-            Lx.append(xc/5)
-            Ly.append(yc2/5)
-    if gen == 3:
-        yc2 = yc+5
-        if (xc/5, yc2/5) in zip(Lx, Ly):
-            able = False
-        else:
-            Lx.append(xc/5)
-            Ly.append(yc2/5)
-            able = True
-            save = yc
-            yc = yc2
-            yc2 = save
-    if able == True:
+    elif gen == 2 or gen == 3:
         create_square(xc, yc, M1, 0)
         create_mon(M1)
         create_square(xc-1, yc, M2, 0)
@@ -211,11 +210,6 @@ def chestroomV(xc, yc, M1, M2, gen):
         create_square(xc+1, yc2+1, "□", 0)
         create_square(xc-1, yc2+2, "C", 0)
         create_square(xc+1, yc2+2, "□", 0)
-    else:
-        if M2 != "□" and M2 != "C":
-            chest_allyV(xc, yc, M2)
-        else:
-            chest_allyV(xc, yc, M1)
 
 def gen(gen):
     map.genID[map.id.index("x")] = 0
@@ -250,109 +244,55 @@ def gen(gen):
             return
         Lx.append(xcentre/5)
         Ly.append(ycentre/5)
-    if gen == 1 or gen == 4:
-        if map.lvl == 1:
-            if room == 1:
-                emptyT(xcentre,ycentre)
-            elif room == 2:
-                chest_allyH(xcentre, ycentre, "K")
-            elif room == 3:
-                central(xcentre, ycentre, "□", "S", "S")
-            elif room == 4:
-                central(xcentre, ycentre, "□", "□", "K")
-            elif room == 5:
-                chest_allyH(xcentre, ycentre, "□")
-            elif room == 6:
-                central(xcentre, ycentre, "C", "S", "K")
-        elif map.lvl == 2:
-            if room == 1:
-                chestroomH(xcentre, ycentre, "□", "F", gen)
-            elif room == 2:
-                emptyT(xcentre,ycentre)
-            elif room == 3:
-                central(xcentre, ycentre, "C", "K", "L")
-            elif room == 4:
-                central(xcentre, ycentre, "L", "□", "□")
-            elif room == 5:
-                chest_allyH(xcentre, ycentre, "L")
-            elif room == 6:
-                central(xcentre, ycentre, "F", "□", "□")
-        elif map.lvl == 3:
-            if room == 1:
-                chestroomH(xcentre, ycentre, "F", "F", gen)
-            if room == 2:
-                central(xcentre, ycentre, "T", "□", "□")
-            if room == 3:
-                central(xcentre, ycentre, "□", "O", "□")
-            if room == 4:
-                emptyT(xcentre, ycentre)
-            if room == 5:
-                chest_allyH(xcentre, ycentre, "T")
-            if room == 6:
-                central(xcentre, ycentre, "□", "O", "O")
-        elif map.lvl >= 4:
-            if room == 1:
-                emptyT(xcentre, ycentre)
-            if room == 2:
-                chest_allyH(xcentre, ycentre, "G")
-            if room == 3:
-                chestroomH(xcentre, ycentre, "□", "G", gen)
-            if room == 4:
-                central(xcentre, ycentre, "Ø", "O", "□")
-            if room == 5:
-                central(xcentre, ycentre, "□", "T", "□")
-            if room == 6:
-                central(xcentre, ycentre, "O", "O", "O")
-    if gen == 2 or gen == 3:
-        if map.lvl == 1:
-            if room == 1:
-                emptyT(xcentre,ycentre)
-            elif room == 2:
-                chest_allyV(xcentre, ycentre, "K")
-            elif room == 3:
-                central(xcentre, ycentre, "□", "S", "S")
-            elif room == 4:
-                central(xcentre, ycentre, "□", "□", "K")
-            elif room == 5:
-                chest_allyV(xcentre, ycentre, "□")
-            elif room == 6:
-                central(xcentre, ycentre, "C", "S", "K")
-        elif map.lvl == 2:
-            if room == 1:
-                chestroomV(xcentre, ycentre, "□", "F", gen)
-            elif room == 2:
-                emptyT(xcentre,ycentre)
-            elif room == 3:
-                central(xcentre, ycentre, "C", "K", "L")
-            elif room == 4:
-                central(xcentre, ycentre, "L", "□", "□")
-            elif room == 5:
-                chest_allyV(xcentre, ycentre, "L")
-            elif room == 6:
-                central(xcentre, ycentre, "F", "□", "□")
-        elif map.lvl == 3:
-            if room == 1:
-                chestroomV(xcentre, ycentre, "F", "F", gen)
-            if room == 2:
-                central(xcentre, ycentre, "T", "□", "□")
-            if room == 3:
-                central(xcentre, ycentre, "□", "O", "□")
-            if room == 4:
-                emptyT(xcentre, ycentre)
-            if room == 5:
-                chest_allyV(xcentre, ycentre, "T")
-            if room == 6:
-                central(xcentre, ycentre, "□", "O", "O")
-        elif map.lvl >= 4:
-            if room == 1:
-                emptyT(xcentre, ycentre)
-            if room == 2:
-                chest_allyH(xcentre, ycentre, "G")
-            if room == 3:
-                chestroomH(xcentre, ycentre, "□", "G", gen)
-            if room == 4:
-                central(xcentre, ycentre, "Ø", "O", "□")
-            if room == 5:
-                central(xcentre, ycentre, "□", "T", "□")
-            if room == 6:
-                central(xcentre, ycentre, "O", "O", "O")
+    if map.lvl == 1:
+        if room == 1:
+            emptyT(xcentre,ycentre)
+        elif room == 2:
+            chest_ally(xcentre, ycentre, "K", gen)
+        elif room == 3:
+            central(xcentre, ycentre, "□", "S", "S")
+        elif room == 4:
+            central(xcentre, ycentre, "□", "□", "K")
+        elif room == 5:
+            chest_ally(xcentre, ycentre, "□", gen)
+        elif room == 6:
+            central(xcentre, ycentre, "C", "S", "K")
+    elif map.lvl == 2:
+        if room == 1:
+            chestroom(xcentre, ycentre, "□", "F", gen)
+        elif room == 2:
+            emptyT(xcentre,ycentre)
+        elif room == 3:
+            central(xcentre, ycentre, "C", "K", "L")
+        elif room == 4:
+            central(xcentre, ycentre, "L", "□", "□")
+        elif room == 5:
+            chest_ally(xcentre, ycentre, "L", gen)
+        elif room == 6:
+            central(xcentre, ycentre, "F", "□", "□")
+    elif map.lvl == 3:
+        if room == 1:
+            chestroom(xcentre, ycentre, "F", "F", gen)
+        if room == 2:
+            central(xcentre, ycentre, "T", "□", "□")
+        if room == 3:
+            central(xcentre, ycentre, "□", "O", "□")
+        if room == 4:
+            emptyT(xcentre, ycentre)
+        if room == 5:
+            chest_ally(xcentre, ycentre, "T", gen)
+        if room == 6:
+            central(xcentre, ycentre, "□", "O", "O")
+    elif map.lvl >= 4:
+        if room == 1:
+            emptyT(xcentre, ycentre)
+        if room == 2:
+            chest_ally(xcentre, ycentre, "G", gen)
+        if room == 3:
+            chestroom(xcentre, ycentre, "□", "G", gen)
+        if room == 4:
+            central(xcentre, ycentre, "Ø", "O", "□")
+        if room == 5:
+            central(xcentre, ycentre, "□", "T", "□")
+        if room == 6:
+            central(xcentre, ycentre, "O", "O", "O")
